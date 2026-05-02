c5543b58257af6fcb0eaf2bc6c5fc79607c3a319
PriyankaN <priyankacn9876@gmail.com>
2026-05-02 22:42:40 +0530

    Replace Student class with student data in JSON format
    Replaced Student class definition with student data.

--- data.json.py
+++ data.json.py
@@ -1,20 +1,10 @@
-# student.py
-# Defines the Student class
-
-class Student:
-    school_name = "NHCE"  # class variable
-
-    def __init__(self, name, grades):
-        self.name = name
-        self.grades = grades
-
-    def average(self):
-        if not self.grades:
-            return 0
-        return sum(self.grades) / len(self.grades)
-
-    def to_dict(self):
-        return {
-            "name": self.name,
-            "grades": self.grades
-        }
+[
+    {
+        "name": "Priyanka",
+        "grades": [80, 90, 85]
+    },
+    {
+        "name": "Asha",
+        "grades": [70, 75, 80]
+    }
+]
